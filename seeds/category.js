988360bd462a8b881ff38b0c49b82953847738eb
tanhd <mrskyhd@gmail.com>
2017-05-28 10:47:18 +0700

    catergory

---
+++ seeds/category.js
@@ -0,0 +1,39 @@
+[
+	{
+		"category_name" : "Thế Giới"
+	},
+	{
+		"category_name" : "Thời Sự"
+	},
+	{
+		"category_name" : "Xã Hội"
+	},
+	{
+		"category_name" :"Văn Hóa"
+	},
+	{
+		"category_name" : "Thể Thao"
+	},
+	{
+		"category_name" : "Kinh Doanh"
+	},
+	{
+		"category_name" : "Du Lịch"
+	},
+	{
+		"category_name" : "Pháp Luật"
+	},
+	{
+		"category_name" : "Sức Khỏe"
+	},
+	{
+		"category_name" : "Bóng Đá"
+	},
+	{
+		"category_name" : "Giải Trí"
+	},
+	{
+		"category_name" : "Tình Yêu"
+	}
+
+]
